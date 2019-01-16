fcb3bd78f6215f037cf0366681b9c3a3ef517a87
Akshay Karai <36170188+akshayKarai@users.noreply.github.com>
2019-01-16 12:16:41 -0500

    Create PermutationStrings.java

---
+++ PermutationStrings.java
@@ -0,0 +1,44 @@
+/* Program to show the possible permutations of given string*/
+
+
+
+
+
+import java.util.Arrays;
+
+public class PermutationStrings 
+{ 
+	public static void main(String[] args) 
+	{ 
+		String str = "ABC"; 
+		int n = str.length(); 
+		PermutationStrings permutation = new PermutationStrings(); 
+		permutation.permute(str, 0, n-1); 
+	} 
+
+	private void permute(String str, int l, int r) 
+	{ 
+		if (l == r) 
+			System.out.println(str); 
+		else
+		{ 
+			for (int i = l; i <= r; i++) 
+			{ 
+				str = swap(str,l,i); 
+				permute(str, l+1, r); 
+				str = swap(str,l,i); 
+			} 
+		} 
+	} 
+
+	public String swap(String a, int i, int j) 
+	{ 
+		char temp; 
+		char[] charArray = a.toCharArray(); 
+		temp = charArray[i] ; 
+		charArray[i] = charArray[j]; 
+		charArray[j] = temp; 
+		return String.valueOf(charArray); 
+	} 
+} 
+
